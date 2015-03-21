494aab670fa17e4dc5b086f56a9e2965ad32980d
SungYongJang <jsy@adain.kr>
2015-03-21 08:25:43 -0700

    update version

--- package.js
+++ package.js
@@ -1,19 +1,15 @@
 Package.describe({
   name: 'adain:meteor-https',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
+  version: '1.0.0',
   summary: 'Simple https proxy server package for meteor using npm http-proxy',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/ADAIN/meteor-https.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Npm.depends({'http-proxy' : '1.9.0'});
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.4.2');
+  api.versionsFrom('1.0.1');
   api.addFiles('https.js');
 });
 
