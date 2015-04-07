dd4d0fb61034b7cbaddf9225ba5911902c873820
SungYongJang <jsy@adain.kr>
2015-04-07 10:22:33 -0700

    add password and ca

--- https.js
+++ https.js
@@ -7,6 +7,8 @@ if(process.env.USE_HTTPS !== undefined && process.env.USE_HTTPS === '1')
 {
   var PATH_TO_KEY = 'server.key',
     PATH_TO_CERT = 'cert.pem',
+    SSL_KEY_PASS = '',
+    PATH_TO_CA = '',
     SSL_PORT = 8443,
     TARGET_IP = '127.0.0.1',
     TARGET_PORT = 3000;
@@ -17,6 +19,14 @@ if(process.env.USE_HTTPS !== undefined && process.env.USE_HTTPS === '1')
 
   if(process.env.SSL_CERT_PATH !== undefined){
     PATH_TO_CERT = process.env.SSL_CERT_PATH;
+  }
+
+  if(process.env.SSL_KEY_PASS !== undefined){
+    SSL_KEY_PASS = process.env.SSL_KEY_PASS;
+  }
+
+  if(process.env.SSL_CA_PATH !== undefined){
+    PATH_TO_CA = process.env.SSL_CA_PATH;
   }
 
   if(process.env.SSL_PORT !== undefined){
@@ -51,10 +61,21 @@ if(process.env.USE_HTTPS !== undefined && process.env.USE_HTTPS === '1')
     secure: true
   };
 
+  if(PATH_TO_CA !== '')
+  {
+    options.ssl.ca = [fs.readFileSync(PATH_TO_CA, 'utf8')];
+  }
+
+  if(SSL_KEY_PASS !== '')
+  {
+    options.ssl.passphrase = SSL_KEY_PASS;
+  }
+
   console.log("=======================================");
   console.log("Start https proxy server.");
   console.log("SSL_KEY_PATH : " + PATH_TO_KEY);
   console.log("SSL_CERT_PATH : " + PATH_TO_CERT);
+  console.log("SSL_CA_PATH : " + PATH_TO_CA);
   console.log("SSL_PORT : " + SSL_PORT);
   console.log("SSL_TARGET_IP : " + TARGET_IP);
   console.log("SSL_TARGET_PORT : " + TARGET_PORT);
